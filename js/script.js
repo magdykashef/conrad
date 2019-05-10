975adf3104661f9e150966978f35b01111694f69
magdykashef <magdy7753@gmail.com>
2019-05-10 20:37:04 +0200

    some minor fixes in the paragraph in folio section

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 var f=document.getElementById("first").addEventListener("click", function(){
-    document.getElementById("one").innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod <br>tempor incididunt ut labore et dolore magna aliqua";
-  });
-
-  var f=document.getElementById("second").addEventListener("click", function(){
     document.getElementById("one").innerHTML = "Creativity redefined. These guys offer a unique approach to every aspect <br> of design and the result is simply stunning.";
   });
 
+  var f=document.getElementById("second").addEventListener("click", function(){
+    document.getElementById("one").innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod <br>tempor incididunt ut labore et dolore magna aliqua";
+  });
 
+  
 
   
